658b00a2ca82d77f4b5614e7fc7bd01afec6f8a2
su2708 <su2708@naver.com>
2023-09-03 04:47:29 +0900

    ArduCAM sample

--- Blackbox_Cam/blackbox.py
+++ Blackbox_Cam/blackbox.py
@@ -1,1 +1,37 @@
-print("Hi")
+import RPi.GPIO as gp
+import os
+
+gp.setwarnings(False)
+gp.setmode(gp.BOARD)
+
+gp.setup(7, gp.OUT)
+gp.setup(11, gp.OUT)
+gp.setup(12, gp.OUT)
+
+
+def main():
+    print('Start testing the camera A')
+    i2c = "i2cset -y 1 0x70 0x00 0x04"
+    os.system(i2c)
+    gp.output(7, False)
+    gp.output(11, False)
+    gp.output(12, True)
+    capture(1)
+    print('Start testing the camera B') 
+    i2c = "i2cset -y 1 0x70 0x00 0x05"
+    os.system(i2c)
+    gp.output(7, True)
+    gp.output(11, False)
+    gp.output(12, True)
+    capture(2)
+    
+def capture(cam):
+    cmd = "libcamera-still -o capture_%d.jpg" % cam
+    os.system(cmd)
+
+if __name__ == "__main__":
+    main()
+
+    gp.output(7, False)
+    gp.output(11, False)
+    gp.output(12, True)
