6f6f0ec237083d1fb76c301b786fe571191aa1a8
su2708 <su2708@naver.com>
2023-11-03 02:48:53 +0900

    Add FPS part in video_test.py

--- video_test.py
+++ video_test.py
@@ -1,11 +1,17 @@
 import cv2
+import time
 from picamera2 import Picamera2
 
 def main():
     # 웹캠 초기화
-    cam = cv2.VideoCapture(2) # 2 or 4
+    cam = cv2.VideoCapture(0) # for rpi3
+    #cam = cv2.VideoCapture(4) # 2 or 4 for rpi4
     cam.set(3, 640)
     cam.set(4, 480)
+    
+    # Initialize FPS
+    frame_count = 0
+    start_time = time.time()
     
     while True:
         success, image = cam.read()
@@ -13,6 +19,18 @@ def main():
             print("Fail to read webcam")
             continue
         image = cv2.flip(image, flipCode=1)
+        
+        end_time = time.time()
+        frame_count += 1
+        
+        time_interval = end_time - start_time
+
+        fps = frame_count / time_interval
+        cv2.putText(image, "FPS: {:.2f}".format(fps), (400, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        
+        frame_count = 0
+        start_time = time.time()
+            
         cv2.imshow('Webcam', image)
         
         if cv2.waitKey(1) == ord('q'):
